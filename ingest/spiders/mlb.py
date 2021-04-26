b95205f6fef65a700c34dd6196450fe000608ede
Gurleen Singh <gurleen@wewager.io>
2021-04-26 17:07:57 -0400

    Fixed date in MLB ingest urls

--- ingest/spiders/mlb.py
+++ ingest/spiders/mlb.py
@@ -9,12 +9,12 @@ class MlbSpider(Spider):
     name = "mlb"
 
     def start_requests(self):
-        yield Request("https://bdfed.stitch.mlbinfra.com/bdfed/transform-mlb-scoreboard?stitch_env=prod&sortTemplate=4&sportId=1&startDate=2021-04-24&endDate=2021-04-24&gameType=E&&gameType=S&&gameType=R&&gameType=F&&gameType=D&&gameType=L&&gameType=W&&gameType=A&language=en&leagueId=104&&leagueId=103&contextTeamId=")
-
         bdt = datetime.today()
         edt = bdt - timedelta(days=1)
         begin, end = bdt.strftime("%Y-%m-%d"), edt.strftime("%Y-%m-%d")
-        yield Request(f"https://statsapi.web.nhl.com/api/v1/schedule?startDate={begin}&endDate={end}&hydrate=team(leaders(categories=[points,goals,assists],gameTypes=[R])),linescore,broadcasts(all),tickets,game(content(media(epg),highlights(scoreboard)),seriesSummary),radioBroadcasts,metadata,decisions,scoringplays,seriesSummary(series)&site=en_nhl&teamId=&gameType=&timecode=")
+
+        yield Request(f"https://bdfed.stitch.mlbinfra.com/bdfed/transform-mlb-scoreboard?stitch_env=prod&sortTemplate=4&sportId=1&startDate={end}&endDate={begin}&gameType=E&&gameType=S&&gameType=R&&gameType=F&&gameType=D&&gameType=L&&gameType=W&&gameType=A&language=en&leagueId=104&&leagueId=103&contextTeamId=")
+        yield Request(f"https://statsapi.web.nhl.com/api/v1/schedule?startDate={end}&endDate={begin}&hydrate=team(leaders(categories=[points,goals,assists],gameTypes=[R])),linescore,broadcasts(all),tickets,game(content(media(epg),highlights(scoreboard)),seriesSummary),radioBroadcasts,metadata,decisions,scoringplays,seriesSummary(series)&site=en_nhl&teamId=&gameType=&timecode=")
 
     def parse(self, response):
         data = json.loads(response.body)
