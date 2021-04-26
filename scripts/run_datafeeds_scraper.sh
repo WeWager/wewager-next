1e28d8408b5425ad6d050dfca87872d2a850f911
Gurleen Singh <gurleen@wewager.io>
2021-04-26 17:29:26 -0400

    Added shebangs to scripts

--- scripts/run_datafeeds_scraper.sh
+++ scripts/run_datafeeds_scraper.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 cd /usr/src/app/ingest/spiders
 scrapy crawl datafeeds
--- scripts/run_sports_scrapers.sh
+++ scripts/run_sports_scrapers.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 cd /usr/src/app/ingest/spiders
 scrapy list | grep -v datafeeds | xargs -n 1 scrapy crawl
