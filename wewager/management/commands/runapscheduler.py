c342afbe9e13c9eb3d61b0443cbade3e1e479184
Gurleen Singh <gurleen@wewager.io>
2021-05-03 18:34:51 -0400

    Cleaned up AP Scheduler management command

--- wewager/management/commands/runapscheduler.py
+++ wewager/management/commands/runapscheduler.py
@@ -9,15 +9,9 @@ from ingest import runner
 
 logger = logging.getLogger(__name__)
 
-"""
-def delete_old_job_executions(max_age=604_800):
-    This job deletes all apscheduler job executions older than `max_age` from the database.
-    DjangoJobExecution.objects.delete_old_job_executions(max_age)
-"""
 
 class Command(BaseCommand):
     help = "Runs apscheduler."
 
     def handle(self, *args, **options):
-        # scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
         runner.run()
