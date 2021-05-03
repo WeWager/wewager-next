d2dec255754ae7a71a61d6f9def7d271da460997
Gurleen Singh <gurleen@wewager.io>
2021-05-03 19:06:14 -0400

    Added point spread resolver

--- wewager/bet_closer.py
+++ wewager/bet_closer.py
@@ -20,24 +20,21 @@ class BetCloser:
 
     @staticmethod
     def resolve_bet(outcome: GameOutcome, game: Game):
-        print(f"Finding resolver for {outcome.bet_type}")
         for resolver in resolvers:
-            print(f"Trying resolver: {resolver}")
             if re.match(resolver, outcome.bet_type):
+                handler = resolvers[resolver]
                 logger.debug(f"Resolver {resolver} matched.")
                 handler(outcome, game)
                 outcome.save()
                 return
-            print(f"ERROR: No resolver matched {outcome.bet_type}")
+            logger.error(f"ERROR: No resolver matched {outcome.bet_type}")
 
     @staticmethod
     def resolve_game(game: Game):
         for outcome in game.outcomes.all():
-            print(f"Resolving {outcome.description}...")
+            logger.debug(f"Resolving {outcome.description}...")
             if outcome.hit is None:
                 BetCloser.resolve_bet(outcome, game)
-            else:
-                print(f"Outcome already calcualted: {outcome.hit}")
             wagers = Wager.objects.filter(outcome=outcome, status=WagerState.ACCEPTED)
             for wager in wagers:
                 wager.complete(outcome.hit)
@@ -65,3 +62,21 @@ def total_points(outcome: GameOutcome, game: Game):
         outcome.hit = total > amount
     elif direction == "Under":
         outcome.hit = total < amount
+
+
+@BetCloser.resolver("(Point Spread|Run Line)")
+def point_spread(outcome: GameOutcome, game: Game):
+    teams = game.data["participants"]
+    direction = "+" if "+" in outcome.description else "-"
+    split = outcome.description.split(direction)
+    team_name, line = split[0].strip(), float(split[1])
+    if direction == "-":
+        line *= -1
+
+    matching_team = next(x for x in teams if x["name"] == team_name)
+    other_team = next(x for x in teams if x != matching_team)
+
+    matching_team["score"] += line
+
+    winner = max((matching_team, other_team), key=lambda x: x["score"])
+    hit = winner == matching_team
