1fa8eb908ea00d750c4dabd7a6f967a2a3bb0776
Gurleen Singh <gurleen@wewager.io>
2021-05-04 23:40:21 -0400

    Added default filter backend

--- wewager_next/settings.py
+++ wewager_next/settings.py
@@ -75,6 +75,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     "PAGE_SIZE": 100,
 }
 
