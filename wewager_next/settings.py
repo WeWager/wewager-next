1982a54f2aae45b921c7edcfd5b04fc46bbb4e1f
Gurleen Singh <gurleen@wewager.io>
2021-05-05 14:33:55 -0400

    Set default page size to 20

--- wewager_next/settings.py
+++ wewager_next/settings.py
@@ -77,7 +77,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 20,
 }
 
 
