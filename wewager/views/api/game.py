3d80c537de741469de5e3d03d6fd098f3c7be95a
Gurleen Singh <gurleen@wewager.io>
2021-05-04 23:40:33 -0400

    Added filters and ordering to Game endpoint

--- wewager/views/api/game.py
+++ wewager/views/api/game.py
@@ -2,17 +2,37 @@ from datetime import datetime
 
 import pytz
 from rest_framework import viewsets
+import django_filters.rest_framework as filters
 
 from wewager.models import Game
 from wewager.serializers import GameSerializer
 
 
+class GameFilter(filters.FilterSet):
+    since = filters.DateTimeFilter(field_name="date", lookup_expr="gte")
+    on = filters.DateTimeFilter(field_name="date", method="get_games_on_date", label="On date")
+
+    ordering = filters.OrderingFilter(
+        fields=(
+            ("date", "date"),
+            ("league", "league")
+        )
+    )
+
+    def get_games_on_date(self, queryset, field_name, value):
+        return queryset.filter(date__date=value.date())
+
+    class Meta:
+        model = Game
+        fields = ("description", "date", "league", "ended", "since", "ordering")
+
+
 class GameViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    /game
-    /game/:id
-    Returns games currently in progress or in the future
+    View current and past games
     """
 
-    queryset = Game.objects.filter(ended=False).order_by("date")
+    queryset = Game.objects.all()
     serializer_class = GameSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = GameFilter
