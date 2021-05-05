ecdbb8d6f0ab27fae879adde04668a4fb522dc03
Gurleen Singh <gurleen@wewager.io>
2021-05-05 14:15:08 -0400

    Moved PostMixins back to PostViewSet

--- social/views/post.py
+++ social/views/post.py
@@ -1,14 +1,59 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from django.db.models import Count, Q, OuterRef, Exists
 
 from social.models import Post
-from .post_mixin import PostMixins
-from social.serializers import PostSerializer
+from social.serializers import PostSerializer, CommentSerializer
+from common.permissions import IsOwnerOrReadOnly, CanReactToPost
 
 
-class PostViewSet(viewsets.ModelViewSet, PostMixins):
+class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, (IsOwnerOrReadOnly | CanReactToPost)]
     
     def get_queryset(self):
-        return Post.objects.all()
+        user = self.request.user
+        liked = Post.objects.filter(id=OuterRef("id"), liked_by=user)
+        return Post.objects.all().annotate(
+                like_count=Count("liked_by"),
+                reply_count=Count("comments"),
+                liked=Exists(liked),
+            ).order_by("-date")
+
+    def action_response(self, post):
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["POST"])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        post.like(self.request.user)
+        return self.action_response(post)
+
+    @action(detail=True, methods=["POST"])
+    def dislike(self, request, pk=None):
+        post = self.get_object()
+        post.dislike(self.request.user)
+        return self.action_response(post)
+
+    @action(detail=True, methods=["GET"])
+    def comment(self, request, pk=None):
+        post = self.get_object()
+        comments = post.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        paginator = PageNumberPagination()
+        paginator.page_size = 10
+        paginator.paginate_queryset(comments, request)
+        return paginator.get_paginated_response(serializer.data)
+
+    @action(detail=True, methods=["POST"])
+    def add_comment(self, request, pk=None):
+        post = self.get_object()
+        context = {"parent": post, "request": request}
+        serializer = CommentSerializer(data=request.data, context=context)
+        if serializer.is_valid(raise_exception=True):
+            comment = serializer.save(user=self.request.user)
+            return self.action_response(serializer.data)
