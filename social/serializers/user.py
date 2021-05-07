b5054a8ce08b0ce1fe2212e8f747fe36b33e6e45
Gurleen Singh <gurleen@wewager.io>
2021-05-07 19:37:30 -0400

    Changed avatar field to URL string, ran black

--- social/serializers/user.py
+++ social/serializers/user.py
@@ -2,7 +2,17 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
+DEFAULT_AVATAR = "https://wewager-next.us-east-1.linodeobjects.com/avatars/default.png"
+
+
 class UserSerializer(serializers.ModelSerializer):
+    avatar_url = serializers.SerializerMethodField()
+
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "first_name", "last_name", "avatar")
+        fields = ("id", "username", "first_name", "last_name", "avatar_url")
+
+    def get_avatar_url(self, user):
+        if hasattr(user, "avatar"):
+            return user.avatar.url
+        return DEFAULT_AVATAR
