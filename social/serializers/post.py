0e7fa6fb21da5572c96aa8ceb1c466b13469d335
Gurleen Singh <gurleen@wewager.io>
2021-05-07 16:12:22 -0400

    Added current_user action, depth to PostSerializer

--- social/serializers/post.py
+++ social/serializers/post.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 
 from social.models import Post
+from social.serializers.user import UserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
     like_count = serializers.IntegerField(read_only=True)
     reply_count = serializers.IntegerField(read_only=True)
     liked = serializers.BooleanField(read_only=True)
-    user = serializers.PrimaryKeyRelatedField(
+    user = UserSerializer(
         read_only=True, default=serializers.CurrentUserDefault()
     )
 
--- social/views/user.py
+++ social/views/user.py
@@ -1,7 +1,17 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import BasePermission
+from rest_framework.response import Response
 
 from social.serializers import UserSerializer
+
+
+class CurrentUserPermission(BasePermission):
+    def has_permission(self, request, view):
+        if view.action == "current_user":
+            return request.user.is_authenticated
+        return True
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -13,3 +23,9 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = get_user_model().objects.all().order_by("id")
     serializer_class = UserSerializer
+    permission_classes = [CurrentUserPermission]
+
+    @action(methods=["GET"], detail=False)
+    def current_user(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
