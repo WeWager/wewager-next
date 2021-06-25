ecbb58c212cb07aad4d34b3f991d9a79c3152745
Gurleen Singh <gurleen@wewager.io>
2021-06-25 17:03:03 -0400

    Added depth to WalletSerializer

--- wewager/serializers/wallet.py
+++ wewager/serializers/wallet.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from wewager.models import Wallet
+from social.serializers.user import UserSerializer
 
 
 class WalletSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Wallet
         fields = (
